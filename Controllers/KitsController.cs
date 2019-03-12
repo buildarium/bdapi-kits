9461ccb3a5bd4db1ab824a9472457d90c6e1a29f
Buck Tower <buck@bucktower.net>
2019-03-12 15:54:14 -0500

    Initial commit

---
+++ Controllers/KitsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bdapi_kits.Controllers
+{
+    [Route("kit")]
+    [ApiController]
+    public class KitsController : ControllerBase
+    {
+        /* User actions */
+
+        // Get your current claimed kits
+        // GET /kit
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // Claim a kit
+        // PUT /kit/51dbd231dfs241dsdae23c4a
+        [HttpPut("{token}")]
+        public void Put(string token, [FromBody] string value)
+        {
+        }
+
+        /* Administrative actions */
+
+        // Add an available kit for sale
+        // POST /kit
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // Remove a kit
+        // DELETE kit/51dbd231dfs241dsdae23c4a
+        [HttpDelete("{token}")]
+        public void Delete(string token)
+        {
+        }
+    }
+}
---
+++ Models/Kit.cs
@@ -0,0 +1,9 @@
+namespace bdapi_kits.Models
+{
+  public class Kit
+  {
+    public int Id { get; set; }
+    public string Token { get; set; }
+    public string Owner { get; set; }
+  }
+}
